1d0bd76fd0ae8bd59281cdcb5a4cf214d65ae530
Marcus Paulo Correa Lima <39959463+sucramlima2021@users.noreply.github.com>
2023-01-24 10:53:35 -0300

    Add files via upload

--- praias/urls.py
+++ praias/urls.py
@@ -1,11 +1,11 @@
 
 from django.urls import path
-from praias.views import lista_praias
+from praias.views import lista_praias, ini
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    
+    path('', ini, name = 'ini'),
     path('lista/', lista_praias, name='lista_praias'),
 ]
 
--- praias/views.py
+++ praias/views.py
@@ -1,4 +1,4 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from praias.models import Praias
 from django.core import serializers
 # Create your views here.
@@ -7,4 +7,8 @@ def lista_praias(request):
     #retorna um objeto json com a listagem das praias
     praias = serializers.serialize('json', Praias.objects.all())
     return JsonResponse(praias, safe=False)
+
+def ini(request):
+   
+    return HttpResponse('<h1>Balneabilidade das praias do Rio de Janeiro.</h1>')
 
